5afb44cecfd413a03f066fd44b55497269841a43
amirmehrabifard <amehrabifard@gmail.com>
2025-06-01 13:37:44 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -87,3 +87,13 @@ def manage_invite(message):
         bot.send_message(inviter_id, "❌ خطا در دریافت شناسه دعوت‌شونده!")
 
 bot.polling()
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Bot is running!"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=PORT)
